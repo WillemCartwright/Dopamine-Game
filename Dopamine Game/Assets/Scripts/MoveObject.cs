8063f484d25a34094358f7deeec2623ed5c7450c
MargoBouwens <mae.bouwens@student.avans.nl>
2024-11-04 14:28:13 +0100

    bril

--- Dopamine Game/Assets/Scripts/MoveObject.cs
+++ Dopamine Game/Assets/Scripts/MoveObject.cs
@@ -1,41 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveObject : MonoBehaviour
 {
     public GameObject item;
     public GameObject tempParent;
     public Transform guide;
+    public Image uiImage;
 
     private bool isHoldingItem = false;
 
     void Start()
     {
         item.GetComponent<Rigidbody>().useGravity = true;
+        uiImage.gameObject.SetActive(false); // Verberg de afbeelding bij het starten van de game
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            // P-toets ingedrukt: begin met het vasthouden van het object
-            isHoldingItem = true;
-            item.GetComponent<Rigidbody>().useGravity = false;
-            item.GetComponent<Rigidbody>().isKinematic = true;
-            item.transform.position = guide.transform.position;
-            item.transform.rotation = guide.transform.rotation;
-            item.transform.parent = tempParent.transform;
+            if (!isHoldingItem)
+            {
+                isHoldingItem = true;
+                uiImage.gameObject.SetActive(true);
+                item.GetComponent<Rigidbody>().useGravity = false;
+                item.GetComponent<Rigidbody>().isKinematic = true;
+                item.transform.position = guide.transform.position;
+                item.transform.rotation = guide.transform.rotation;
+                item.transform.parent = tempParent.transform;
+            }
+            else
+            {
+                isHoldingItem = false;
+                uiImage.gameObject.SetActive(false);
+                item.GetComponent<Rigidbody>().useGravity = true;
+                item.GetComponent<Rigidbody>().isKinematic = false;
+                item.transform.parent = null;
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.P))
+        if (isHoldingItem)
         {
-            // P-toets losgelaten: laat het object los
-            isHoldingItem = false;
-            item.GetComponent<Rigidbody>().useGravity = true;
-            item.GetComponent<Rigidbody>().isKinematic = false;
-            item.transform.parent = null;
             item.transform.position = guide.transform.position;
+            item.transform.rotation = guide.transform.rotation;
         }
     }
 }
