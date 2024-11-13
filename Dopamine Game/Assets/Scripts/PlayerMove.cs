d3ae46ed28aa8a7a7424575662b0ec88f5655849
WillemCartwright <willem1cartwright@gmail.com>
2024-11-13 20:24:31 +0100

    sounds enzo

--- Dopamine Game/Assets/Scripts/PlayerMove.cs
+++ Dopamine Game/Assets/Scripts/PlayerMove.cs
@@ -1,9 +1,3 @@
-/* 
- * PLAYER MOVE
- * Moves the Player object according to key inputs.
- * Crouching and jumping are optional
- */
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,11 +8,11 @@ public class PlayerMove : MonoBehaviour
     public float walkingSpeed = 3.0f;
 
     public bool jumpEnabled;
-
     public float jumpSpeed = 1.0f;
     public bool crouchEnabled;
     public float crouchHeight = 0.4f;
     private float normalHeight;
+    
     public KeyCode forwardKey = KeyCode.W;
     public KeyCode backKey = KeyCode.S;
     public KeyCode leftKey = KeyCode.A;
@@ -26,10 +20,19 @@ public class PlayerMove : MonoBehaviour
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode crouchKey = KeyCode.LeftControl;
 
+    private AudioSource audioSource;  // Voeg de AudioSource toe
+    public AudioClip walkingClip;     // Sleep hier je loopgeluid in de inspector
+    private bool isWalking = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         normalHeight = transform.localScale.y;
+        
+        // Haal de AudioSource op en stel het geluid in
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = walkingClip;
+        audioSource.loop = true; // Zorg ervoor dat het geluid in een loop wordt afgespeeld
     }
 
     void FixedUpdate()
@@ -43,23 +46,32 @@ public class PlayerMove : MonoBehaviour
             movement += transform.forward * walkingSpeed;
             hasInput = true;
         }
-
         if (Input.GetKey(backKey))
         {
             movement += -transform.forward * walkingSpeed;
             hasInput = true;
         }
-
         if (Input.GetKey(rightKey))
         {
             movement += transform.right * walkingSpeed;
             hasInput = true;
         }
-
         if (Input.GetKey(leftKey))
         {
             movement += -transform.right * walkingSpeed;
             hasInput = true;
+        }
+
+        // Controleren of het loopgeluid moet worden afgespeeld
+        if (hasInput && !isWalking)
+        {
+            audioSource.Play();
+            isWalking = true;
+        }
+        else if (!hasInput && isWalking)
+        {
+            audioSource.Pause();
+            isWalking = false;
         }
 
         // Jumping
@@ -68,40 +80,39 @@ public class PlayerMove : MonoBehaviour
             movement += transform.up * jumpSpeed;
         }
 
-        // make sure the rigidbody isn't sliding around when there's no input
+        // Beperk beweging als er geen input is
         if (!hasInput) {
-          rb.constraints = 
-            RigidbodyConstraints.FreezePositionX | 
-            RigidbodyConstraints.FreezePositionZ |
-            RigidbodyConstraints.FreezeRotationY |
-            RigidbodyConstraints.FreezeRotationZ;
+            rb.constraints = 
+                RigidbodyConstraints.FreezePositionX | 
+                RigidbodyConstraints.FreezePositionZ |
+                RigidbodyConstraints.FreezeRotationY |
+                RigidbodyConstraints.FreezeRotationZ;
         } else {
-          rb.constraints = 
-            RigidbodyConstraints.FreezeRotationY |
-            RigidbodyConstraints.FreezeRotationZ;
+            rb.constraints = 
+                RigidbodyConstraints.FreezeRotationY |
+                RigidbodyConstraints.FreezeRotationZ;
         }
 
-        // maintain vertical speed
+        // Handhaaf verticale snelheid
         movement.y += rb.velocity.y;
 
-        // apply movement to rigidbody
-        rb.velocity = movement ;
+        // Pas de snelheid aan de Rigidbody toe
+        rb.velocity = movement;
     }
-
-    
 
     void Update() {
         if (crouchEnabled && Input.GetKeyDown(crouchKey)) {
-          // Crouching
-          transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
+            // Crouching
+            transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
         } else if (crouchEnabled && Input.GetKeyUp(crouchKey)) {
-          // Not crouching
-          transform.localScale = new Vector3(transform.localScale.x, normalHeight, transform.localScale.z);
+            // Not crouching
+            transform.localScale = new Vector3(transform.localScale.x, normalHeight, transform.localScale.z);
         }
     }
 
     // Check if player is on the ground
     bool isGrounded() {
-      return Physics.Raycast(transform.position, -Vector3.up, 0.1f + transform.localScale.y);
+        return Physics.Raycast(transform.position, -Vector3.up, 0.1f + transform.localScale.y);
     }
 }
+
